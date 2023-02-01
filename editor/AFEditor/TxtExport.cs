3569e8b92c2b5786f6dea8ceabba807b1e6f3a59
Makcim Ivanov <makcimbx@gmail.com>
2023-02-01 19:59:07 +0300

    Попытка в UTF-8 Едитор

--- editor/AFEditor/TxtExport.cs
+++ editor/AFEditor/TxtExport.cs
@@ -10,7 +10,7 @@ namespace AFEditor
     /**
      * TXT file export/import
      * 
-     * Encoding: SHIFT-JIS
+     * Encoding: UTF-8
      */
     public class TxtExport
     {
@@ -26,7 +26,7 @@ namespace AFEditor
         {
             FileStream fs = new FileStream(filename, FileMode.Open);
 
-            StreamReader reader = new StreamReader(fs, Encoding.GetEncoding("SHIFT-JIS"));
+            StreamReader reader = new StreamReader(fs, Encoding.GetEncoding("UTF-8"));
 
             bool english = false;
             StringBuilder engText = new StringBuilder();
@@ -50,7 +50,7 @@ namespace AFEditor
                         jpnText.Replace("\r\n", "\n");
                         jpnText.Remove(jpnText.Length - 1, 1);
                         jpnText.Append('\x00');
-                        byte[] source = Encoding.GetEncoding("SHIFT-JIS").GetBytes(jpnText.ToString());
+                        byte[] source = Encoding.GetEncoding("UTF-8").GetBytes(jpnText.ToString());
 
                         try
                         {
@@ -94,7 +94,7 @@ namespace AFEditor
         {
             FileStream fs = new FileStream(filename, FileMode.Create);
 
-            StreamWriter writer = new StreamWriter(fs, Encoding.GetEncoding("SHIFT-JIS"));
+            StreamWriter writer = new StreamWriter(fs, Encoding.GetEncoding("UTF-8"));
 
             foreach (KeyValuePair<UInt32, PackageText> kv in resman.dialogue)
             {
