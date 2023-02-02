cd531ad92c82780d4fe0aacbe7d5d65ed93eceb0
MishaIac <47920105+MishaIac@users.noreply.github.com>
2023-02-02 18:09:27 +0200

    Чистка

--- CGameFunctions.cpp
+++ CGameFunctions.cpp
@@ -1,9 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "CGameFunctions.h"
 
-bool CGameFunctions::m_fixCharacters = false;
-char CGameFunctions::m_lastLine[TEXT_BUFFER];
-int  CGameFunctions::m_lastLinePosition = 0;
 char CGameFunctions::m_textBuffer[TEXT_BUFFER];
 
 CResourceManager* CGameFunctions::m_resources = NULL;
@@ -15,23 +12,10 @@ const char* __fastcall CGameFunctions::TranslateText(void* a1, void* a2, void* a
 {
 	auto text = Orig_TranslateText(a1, a2, a3);
 
-	if (*text == '\x00' || (*text == '\x05' && *(text + 1) == '\x00'))
-	{
-		return text;
-	}
-
-	m_fixCharacters = false;
-	strncpy(m_lastLine, text, TEXT_BUFFER);
-	m_lastLinePosition = 0;
 	bool result = m_resources->TranslateText(text, m_textBuffer, TEXT_BUFFER);
 
 	if (result)
 	{
-		if (m_lastLine[0] == '\x81' && m_lastLine[1] == '\x79')
-		{
-			m_fixCharacters = true;
-		}
-
 		return m_textBuffer;
 	}
 	else
@@ -42,23 +26,10 @@ const char* __fastcall CGameFunctions::TranslateText(void* a1, void* a2, void* a
 
 void __fastcall CGameFunctions::TranslateTextUI(void* a1, const char* text)
 {
-	if (*text == '\x00' || (*text == '\x05' && *(text + 1) == '\x00'))
-	{
-		return Orig_TranslateTextUI(a1, text);
-	}
-
-	m_fixCharacters = false;
-	strncpy(m_lastLine, text, TEXT_BUFFER);
-	m_lastLinePosition = 0;
 	bool result = m_resources->TranslateUserInterface(text, m_textBuffer, TEXT_BUFFER);
 
 	if (result)
 	{
-		if (m_lastLine[0] == '\x81' && m_lastLine[1] == '\x79')
-		{
-			m_fixCharacters = true;
-		}
-
 		return Orig_TranslateTextUI(a1, m_textBuffer);
 	}
 	else
--- CGameFunctions.h
+++ CGameFunctions.h
@@ -7,19 +7,14 @@
 
 class CGameFunctions {
 public:
-	// Common functions
 	static const char* __fastcall TranslateText(void* a1, void* a2, void* a3);
 	static void __fastcall TranslateTextUI(void* a1, const char* text);
 
-	// rUGP real functions
 	static const char* (__fastcall* Orig_TranslateText) (void*, void*, void*);
 	static void (__fastcall* Orig_TranslateTextUI) (void*, const char*);
 
 	static CResourceManager* m_resources;
 
 private:
-	static bool m_fixCharacters;
-	static char m_lastLine[TEXT_BUFFER];
-	static int  m_lastLinePosition;
 	static char m_textBuffer[TEXT_BUFFER];
 };
